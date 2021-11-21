3df6c76431dc6f17d7d3817daf429035322a794e
nemenek <markbodri@gmail.com>
2021-11-21 16:15:39 +0100

    Implemented my exception class.

--- Scripting/Scripting/Headers/Expressions/Expressions.h
+++ Scripting/Scripting/Headers/Expressions/Expressions.h
@@ -3,6 +3,14 @@
 #include <stack>
 #include <map>
 
+class ExpressionsException{
+public:
+	ExpressionsException(std::string msg);
+	const char* what();
+private:
+	std::string message;
+};
+
 class Expressions {
 public:
 	Expressions(std::string sourceFilePathint);
@@ -10,4 +18,5 @@ private:
 	int Execute(std::string commandName, std::string value);
 	std::map <std::string, float> variables;
 	float GetNextValue(std::string str);
+	bool CheckIfNum(std::string str);
 };
