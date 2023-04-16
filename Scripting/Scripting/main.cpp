f253bb33c815819dc43a1a7d6ea1ed961a22c9a6
sultgalu <komlosi.norbii@gmail.com>
2023-04-16 14:21:06 +0200

    sample for host read/write

--- Scripting/Scripting/main.cpp
+++ Scripting/Scripting/main.cpp
@@ -1,30 +1,54 @@
 #include "Headers/Expressions/VirtualMachine.h"
 #include <iostream>
 
+struct vec3 {
+	float x, y, z;
+};
+
+struct Transform {
+	vec3 pos;
+	vec3 scale;
+};
+
+static VirtualMachine vm;
+static Transform tr;
+
+void initTransformStruct() {
+	vm.initializeStruct("vec3");
+	vm.addFieldToStruct("vec3", "x", 0);
+	vm.addFieldToStruct("vec3", "y", 0);
+	vm.addFieldToStruct("vec3", "z", 0);
+}
+
+void getTransform(std::string name) {
+	vm.initializeStruct(name);
+	vm.addStructFieldToStruct(name, "pos", "vec3");
+	vm.addStructFieldToStruct(name, "scale", "vec3");
+	// vm.setField(name + ".pos.x", tr.pos.x);
+	// vm.setField(name + ".pos.y", tr.pos.y);
+	// vm.setField(name + ".pos.z", tr.pos.z);
+	// vm.setField(name + ".scale.x", tr.scale.x);
+	// vm.setField(name + ".scale.y", tr.scale.y);
+	// vm.setField(name + ".scale.z", tr.scale.z);
+}
+
+void writeTransform(std::string name) {
+	// tr.pos.x = vm.getField(name + ".pos.x");
+	// tr.pos.y = vm.getField(name + ".pos.y");
+	// tr.pos.z = vm.getField(name + ".pos.z");
+	// tr.scale.x = vm.getField(name + ".scale.x");
+	// tr.scale.y = vm.getField(name + ".scale.y");
+	// tr.scale.z = vm.getField(name + ".scale.z");
+}
+
 int main() {
-	void writeIt(std::string param);
-	void addNums(float first, float second);
-
-	VirtualMachine vm;
-	//external functions
-	vm.initializeEnum("Animal");
-	vm.addEnumType("Animal", "Cat");
-	vm.addEnumType("Animal", "Dog");
-	vm.addEnumType("Animal", "Monkey");
-
+	initTransformStruct();
+	vm.addExternalFunction("getTransform", getTransform);
+	vm.addExternalFunction("writeTransform", writeTransform);
 	try {
 		vm.executeFromFile("sample.nk");
 	}
 	catch (VirtualMachineException e) {
 		std::cout << e.what() << std::endl;
 	}
-}
-
-void writeIt(std::string param) {
-	std::cout <<  param << std::endl;
-}
-
-void addNums(float first, float second) {
-	float result = first + second;
-	std::cout << result << std::endl;
 }
